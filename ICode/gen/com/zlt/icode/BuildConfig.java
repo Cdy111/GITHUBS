9eed0f301e9f0f16a04125d0a64f94707fbc8341
allencall1234 <809832192@163.com>
2015-12-01 16:38:35 +0800

    Delete no useful files

--- ICode/gen/com/zlt/icode/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.zlt.icode;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- ICode/快速更新数据库脚本.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Users\Administrator\Desktop\db
-del infos.db if exit
-adb remount
-adb pull /data/data/com.zlt.icode/databases/infos.db C:\Users\Administrator\Desktop\db
